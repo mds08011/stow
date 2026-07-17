13916d328bbe210b4425e0362b8aaa16f84e3d61
Malcolm Smith <malcolmsmith18@gmail.com>
2026-07-16 22:10:35 -0700

    Bump version to 2.4 (versionCode 13).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.stow"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.3"
+        versionCode = 13
+        versionName = "2.4"
     }
 
     buildTypes {
